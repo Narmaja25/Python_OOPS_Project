869e2c69bb8c39fbfc36582d40dec80697b25060
Narmaja Medarametla <narmajauppalapati@gmail.com>
2026-09-23 13:36:33 +0530

    Polymorphism & Abstraction

---
+++ day2.py
@@ -0,0 +1,35 @@
+name ="Rahul"
+age = 20
+city = "Hyderabad"
+#print ("Name:", name)
+#print ("Age:", age)
+#print ("City:", city)
+student_name = "Anjali"
+student_age = 15
+student_marks = 92.5
+#print(student_name)
+#print(student_age)
+#print(student_marks)
+name = "Amit"
+age = 22
+#print ("Name:", name)
+#print ("Age:", age)
+#print("welcome to Python")
+#print("I am Learning Python Programming")
+#print("Python is easy to learn")
+name = " Suresh"
+age = 19
+height = 5.8
+is_student = True
+#print("Name:", name)
+#print("Age:", age)
+#print("Height:", height)
+#print("Is Student:", is_student)
+name, age, city = "Rahul", 21, "Hyderabad"
+#print("Name:", name)
+#print("Age:", age)
+#print("City:", city)
+age = 21
+#print("Original age:", age)
+age = 25
+#print("Reassigned age:", age)
